ce18d79d84c534d447005a69dcc923df8e78f531
Khanh5604 <biphingua@gmail.com>
2025-03-16 19:27:34 +0700

    bo sung

--- Tuan3_bai2/app/src/main/java/com/example/tuan3_bai2/MainActivity.kt
+++ Tuan3_bai2/app/src/main/java/com/example/tuan3_bai2/MainActivity.kt
@@ -1,20 +1,21 @@
 package com.example.tuan3_bai2
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(R.layout.activity_main) // Hiển thị giao diện MainActivity
+
+        // Chuyển sang OpenActivity sau 3 giây
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, OpenActivity::class.java)
+            startActivity(intent)
+            finish() // Đóng MainActivity
+        }, 3000)
     }
 }
