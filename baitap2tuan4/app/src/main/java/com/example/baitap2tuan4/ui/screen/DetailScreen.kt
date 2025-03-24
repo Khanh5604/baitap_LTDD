efc35bfc7d10495d2859dba60611fb27409e03a3
Khanh5604 <biphingua@gmail.com>
2025-03-25 03:05:51 +0700

    hoàn thiện

--- baitap2tuan4/app/src/main/java/com/example/baitap2tuan4/ui/screen/DetailScreen.kt
+++ baitap2tuan4/app/src/main/java/com/example/baitap2tuan4/ui/screen/DetailScreen.kt
@@ -1,7 +1,10 @@
 package com.example.baitap2tuan4.ui.screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,11 +16,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.style.TextAlign
 import androidx.navigation.compose.rememberNavController
 
@@ -28,40 +33,71 @@ fun DetailScreen(navController: NavController) {
         topBar = {
             TopAppBar(
                 title = {
-                    Text(
-                        text = "Detail Screen",
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color.Magenta
-                    )
+                    Box(
+                        modifier = Modifier.fillMaxWidth(),
+                    ) {
+                        Text(
+                            text = "Detail Screen",
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold,
+                            textAlign = TextAlign.Center,
+                            color = Color.Magenta,
+                            modifier = Modifier.align(Alignment.Center)
+
+                            )
+                    }
                 },
                 navigationIcon = {
                     IconButton(onClick = { navController.popBackStack() }) {
                         Icon(
                             imageVector = Icons.Default.ArrowBack,
                             contentDescription = "Back",
-                            tint = Color.Black
+                            tint = Color.White
                         )
                     }
                 },
-                colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = Color.Blue)
+                colors = TopAppBarDefaults.mediumTopAppBarColors()
             )
-        }
+        },
+                contentColor = Color.White
+
     ) { paddingValues ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues),
-            verticalArrangement = Arrangement.SpaceBetween
+            verticalArrangement = Arrangement.SpaceBetween // Điều này đảm bảo nút ở dưới cùng
         ) {
+            // Nội dung chi tiết
             Text(
                 text = "“The only way to do great work is to love what you do”",
                 fontSize = 24.sp,
+                color = Color.White,
                 textAlign = TextAlign.Center,
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(16.dp) // Padding cho text
             )
+
+            // Cột chứa Box với text giữa
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .fillMaxHeight(0.6f)
+                    .padding(16.dp)
+                    .background(Color.LightGray)
+                    .align(Alignment.CenterHorizontally)
+            ) {
+                Text(
+                    text = "“The only way to do great work is to love what you do”",
+                    fontSize = 35.sp,
+                    color = Color.Black,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .fillMaxWidth(0.8f)
+                )
+            }
 
             // Nút PUSH ở dưới cùng
             Button(
@@ -69,10 +105,12 @@ fun DetailScreen(navController: NavController) {
                     navController.navigate("Openscreen") // Đảm bảo đường dẫn này là đúng
                 },
                 modifier = Modifier
-                    .fillMaxWidth(0.85f) // Chiếm 85% chiều rộng
+                    .fillMaxWidth(0.85f)
                     .height(70.dp)
-                    .padding(16.dp), // Padding cho nút
-                shape = RoundedCornerShape(16.dp)
+                    .padding(16.dp)
+                    .align(Alignment.CenterHorizontally),
+
+            shape = RoundedCornerShape(16.dp)
             ) {
                 Text(text = "BACK TO ROOT", fontSize = 18.sp)
             }
@@ -86,3 +124,4 @@ fun PreviewDetailScreen() {
     val fakeNavController = rememberNavController()
     DetailScreen(navController = fakeNavController)
 }
+
